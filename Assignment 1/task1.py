95d51d1d83c07f52f16e079536436fdfa11981ae
Gaurav Kumar <gkm0243@gmail.com>
2026-02-05 12:52:10 +0530

    Add files via upload

---
+++ Assignment 1/task1.py
@@ -0,0 +1,18 @@
+a = int(input("Enter First Number :"))
+b = int(input("Enter Sceond Number :"))
+
+#Addition
+Add = a+b
+print("addition of a and b is:",Add)
+
+#Subtraction
+sub = a-b
+print("subtraction of a and b is:",sub)
+
+# Multiplication
+mult = a*b
+print("multiplication of a and b is:",mult)
+
+# Division
+div = a/b
+print("Division of a and b is:",div)
---
+++ Assignment 1/task2.py
@@ -0,0 +1,4 @@
+First_name = input("Enter First Name :")
+Last_name = input("Enter last Name :")
+
+print("Hello,",First_name,Last_name,"Welcome to the Python program.")
